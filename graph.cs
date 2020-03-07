82756c88b774658b10c21f19f90a2ec521be3da3
Florencia Wijaya <florenciawijaya11@gmail.com>
2020-03-07 17:33:32 +0700

    fix graph

--- graph.cs
+++ graph.cs
@@ -2,27 +2,27 @@ using System;
 using System.Collections.Generic;
 
 class Graph{
-    Dictionary<char,Tuple<char,int>> graf;
+    Dictionary<string,Tuple<string,int>> graf;
 
-    public Graph(char[] node){
+    public Graph(string[] node){
         int i;
-        graf = new Dictionary<char,Tuple<char,int>>();
+        graf = new Dictionary<string,Tuple<string,int>>();
         for(i=0;i<node.Length;i++){
-            graf.Add(node[i],new Tuple<char,int>('0',0));
+            graf.Add(node[i],new Tuple<string,int>("0",0));
         }
     }
 
-    public void AddEdge(char City1, char City2, int weight){
+    public void AddEdge(string City1, string City2, int weight){
         if(graf.ContainsKey(City1)){
             graf.Remove(City1);
         }
-        graf.Add(City1, new Tuple<char,int>(City2,weight));
+        graf.Add(City1, new Tuple<string,int>(City2,weight));
     }
 
     static public void Main(){
-        Graph g = new Graph(new char[] {'a','b','c'});
-        g.AddEdge('a','b',2);
-        g.AddEdge('a','c',1);
-        g.AddEdge('b','c',5);
+        Graph g = new Graph(new string[] {"a","b","c"});
+        g.AddEdge("a","b",2);
+        g.AddEdge("a","c",1);
+        g.AddEdge("b","c",5);
     }
 }
