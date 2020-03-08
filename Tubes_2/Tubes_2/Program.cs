d0350ed30c0c29a0d459a2178a35225e57e1f6a9
Jun <cjunho9868@gmail.com>
2020-03-08 09:20:03 +0700

    change path

--- Tubes_2/Tubes_2/Program.cs
+++ Tubes_2/Tubes_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Tubes_2
 {
@@ -7,8 +8,13 @@ namespace Tubes_2
         public static void Main()
         {
             ReadFile Read = new ReadFile();
-            Read.input_1("E:\\Learning\\semester_4\\stima\\tubes2\\ConsoleApp1\\ConsoleApp1\\input_1.txt");
-            Read.input_2("E:\\Learning\\semester_4\\stima\\tubes2\\ConsoleApp1\\ConsoleApp1\\input_2.txt");
+            string t = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string cur_dir = Directory.GetParent(t).Parent.FullName;
+
+            
+
+            Read.input_1(cur_dir + "\\tubes_2\\input_1.txt");
+            Read.input_2(cur_dir + "\\tubes_2\\input_2.txt");
 
             Graph g = new Graph(Read.getCity(), Read.getPopulation());
             g.BatchEdge(Read.getCityParent(), Read.getCityChild(), Read.getTr());
